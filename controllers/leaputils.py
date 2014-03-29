6ea18d0e46adef74f4d51f13578da239436c5c5e
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-03-29 23:55:24 +0100

    FUCKING TABS

--- controllers/leaputils.py
+++ controllers/leaputils.py
@@ -8,14 +8,14 @@ MAX_Y = 350
 MAX_Z = 150
 
 def rescale_position(old):
-	"""
-	Fit the given vector back into the centered coordinate system.
-	Return a vector with coordinates values in [-1;1]
-	"""
-	# Translate
-	new = old - CENTERED_ORIGIN
-	# Scale
-	new.x /= MAX_X
-	new.y /= MAX_Y
-	new.z /= MAX_Z
-	return new
+    """
+    Fit the given vector back into the centered coordinate system.
+    Return a vector with coordinates values in [-1;1]
+    """
+    # Translate
+    new = old - CENTERED_ORIGIN
+    # Scale
+    new.x /= MAX_X
+    new.y /= MAX_Y
+    new.z /= MAX_Z
+    return new
