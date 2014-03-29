74d4bc21c0e04a838e0763121b4dbddd71339fb8
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-03-29 15:16:03 +0100

    server stuff

--- communication/server.py
+++ communication/server.py
@@ -1,18 +1,31 @@
 import os
+import json
 import socket
 
 socket_path = 'server.sock'
 
-class SocketServer(object):
+class CommandServer(object):
     def __init__(self):
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
 
     def __enter__(self):
-        self.sock.bind(socket_path)
+        self.setup()
 
     def __exit__(self, *args, **kwargs):
+        self.cleanup()
+
+    def setup(self):
+        self.sock.bind(socket_path)
+        self.sockfile = self.sock.makefile()
+
+    def cleanup(self):
         self.sock.shutdown(socket.SHUT_RDWR)
         self.sock.close()
         os.remove(socket_path)
 
-server_socket = SocketServer()
+    def send_command(self, name, data):
+        data['__cmd__'] = name
+        json.dump(data, self.sockfile)
+        self.sockfile.flush()
+
+server_socket = CommandServer()
