659f9d1d4dc4032a6543692c1e7c86ec86290ff5
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-03-29 14:15:49 +0100

    Move and separate controllers

---
+++ controllers/__init__.py
@@ -0,0 +1,38 @@
+import os, sys
+this_dir = os.path.dirname(os.path.realpath(__name__))
+sys.path.insert(0, os.path.join(this_dir, '..', 'lib'))
+import Leap
+from sculpt import SculptListener
+
+_leap_controller = Leap.Controller()
+_current_controller = []
+
+def disable_current_controller():
+    global _current_controller
+    for lstn in _current_controller:
+        _leap_controller.remove_listener(lstn)
+
+def set_current_controller(listener_clss):
+    """
+    Set the current controller (mode), taking a list of listener classes.
+    """
+    global _current_controller
+    disable_current_controller()
+    _current_controller = []
+    for lstn_cls in listener_clss:
+        lstn = lstn_cls()
+        _current_controller.append(lstn)
+        _leap_controller.add_listener(lstn)
+
+def main():
+    set_current_controller([SculptListener])
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    sys.stdin.readline()
+
+    # Remove the sample listener when done
+    disable_current_controller()
+
+if __name__ == '__main__':
+    main()
--- controllers/sculpt.py
+++ controllers/sculpt.py
@@ -1,15 +1,15 @@
-import os
-import sys
-
+import os, sys
 this_dir = os.path.dirname(os.path.realpath(__name__))
-sys.path.insert(0, os.path.join(this_dir, 'lib'))
-
+sys.path.insert(0, os.path.join(this_dir, '..', 'lib'))
 import Leap
-from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+from Leap import ScreenTapGesture
 
 class SculptListener(Leap.Listener):
     def on_init(self, controller):
         controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
+
+    def on_exit(self, controller):
+        pass#controller.disable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
 
     def on_frame(self, controller):
         frame = controller.frame()
@@ -38,36 +38,3 @@ class SculptListener(Leap.Listener):
 
     def screen_tap(self, gesture):
         print 'Screen tapped' # TODO finir
-
-_leap_controller = Leap.Controller()
-_current_controller = []
-
-def disable_current_controller():
-    global _current_controller
-    for lstn in _current_controller:
-        _leap_controller.remove_listener(lstn)
-
-def set_current_controller(listener_clss):
-    """
-    Set the current controller (mode), taking a list of listener classes.
-    """
-    global _current_controller
-    disable_current_controller()
-    _current_controller = []
-    for lstn_cls in listener_clss:
-        lstn = lstn_cls()
-        _current_controller.append(lstn)
-        _leap_controller.add_listener(lstn)
-
-def main():
-    set_current_controller([SculptListener])
-
-    # Keep this process running until Enter is pressed
-    print "Press Enter to quit..."
-    sys.stdin.readline()
-
-    # Remove the sample listener when done
-    disable_current_controller()
-
-if __name__ == "__main__":
-    main()
