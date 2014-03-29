09bf569e72a9ff80f3b1817bae8b2f8aed60bbe4
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-03-29 23:16:05 +0100

    LES MACS C4EST NUL

--- server.py
+++ server.py
@@ -8,7 +8,8 @@ from controllers import (set_current_controller, disable_current_controller,
         SculptListener, GrabListener)
 from communication import clients
 from communication import send_command
-from voice import VoiceRecognition
+if os.name != 'mac'
+    from voice import VoiceRecognition
 
 if __name__ == '__main__':
     socket_path = 'server.sock'
@@ -16,8 +17,9 @@ if __name__ == '__main__':
     sock.bind(socket_path)
     sock.listen(0)
 
-    vr = VoiceRecognition()
-    vr.start()
+    if os.name != 'mac'
+        vr = VoiceRecognition()
+        vr.start()
 
     set_current_controller([GrabListener])
 
