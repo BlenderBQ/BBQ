4f24eefb78336072a8f466b82efd8504e8347719
Gustave Monod <gustave.monod@insa-lyon.fr>
2014-03-30 03:44:43 +0200

    Better includes for Mac.

--- voice/__init__.py
+++ voice/__init__.py
@@ -1,2 +1,5 @@
-from recognition import VoiceRecognition
+# Mac not-imports
+import platform
+if not platform.mac_ver()[0]:
+	from recognition import VoiceRecognition
 from commands import interpret_command
