60d661c9b510f79d1fe975cd4382c01b29558415
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-03-31 10:58:28 +0200

    Make server *much* cooler with more options

--- server.py
+++ server.py
@@ -1,22 +1,33 @@
+"""
+Usage:
+    python2.7 server.py [options]
+
+Options:
+    -h, --help      show this screen
+    --debug         run in debug mode (display network commands)
+    --interactive   run in interactive mode, allowing voice commands to be entered via stdin
+    --no-vr         run without voice recognition (automatic if not available)
+"""
+
 import os, sys
+import socket
+import threading
+
+# leap python binding
 this_dir = os.path.dirname(os.path.realpath(__name__))
 sys.path.insert(0, os.path.join(this_dir, 'lib'))
 import Leap
 
-import socket
-import threading
-
-from controllers import (set_current_controller, disable_current_controller,
-        SculptListener, GrabListener, ScaleListener, CalmGestureListener,
-        PotteryListener)
-from communication import clients, send_command
+from controllers import set_current_controller, disable_current_controller
 import communication as com
 from commands import interpret_command
 
 # Mac not-imports
-import platform
-if not platform.mac_ver()[0]:
+try:
     from voice import VoiceRecognition
+    vr_available = True
+except ImportError:
+    vr_available = False
 
 def run_server():
     print 'Started: Ctrl-C or command \'exit\' to kill.\n'
@@ -24,29 +35,31 @@ def run_server():
         while True:
             pipe, _ = sock.accept()
             pipe.settimeout(0.05)
-            clients.append(pipe)
+            com.clients.append(pipe)
     except KeyboardInterrupt:
         pass
 
 def cleanup_server():
     disable_current_controller()
     sock.close()
-    for pipe in clients:
+    for pipe in com.clients:
         pipe.close()
 
 if __name__ == '__main__':
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # These options enable the same address and port to be reused quickly
-    # (for example, in two consecutive runs of the script)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    sock.bind(('', 1337))
-    sock.listen(0)
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print __doc__
+        sys.exit(0)
 
     # debugging
     com.debug = '--debug' in sys.argv
 
-    if not platform.mac_ver()[0]:
+    # setup server
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('', 1337))
+    sock.listen(0)
+
+    if vr_available and '--no-vr' not in sys.argv:
         vr = VoiceRecognition()
         vr.start()
 
